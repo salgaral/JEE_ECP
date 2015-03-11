89b799d9111375d3e04a1dee4e369e0d7d08e3af
Mari Cruz <Mari Cruz@138.4.192.127>
2015-03-11 10:15:32 +0100

    Implementado el hola de JavaServerFace

---
+++ src/main/java/es/miw/upm/web/jsf/HelloJSFBean.java
@@ -0,0 +1,12 @@
+package es.miw.upm.web.jsf;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class HelloJSFBean {
+    private String name="Hola, desde Bean. OK!!!";
+
+    public String getName() {
+        return name;
+    }
+}
